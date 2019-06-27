8a915614ccf81859dd833d11da34b9d0d31e25f9
Joseph Magsajo <magsajo.joseph@gmail.com>
2019-06-27 21:52:05 +0800

    new commit

--- app.js
+++ app.js
@@ -53,8 +53,16 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a new note',
-    handler: function(){
-        console.log('Removing the note!!');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        // console.log('Removing the note!!');
+        notes.removeNote(argv.title)
     }
 });
 
--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = function(){
     return 'Notess...';
@@ -41,7 +42,22 @@ const loadNotes = function(){
     
 }
 
+const removeNote = function(title){
+    const notes = loadNotes();
+    const removedNote = notes.filter(function(note){
+        return note.title !== title;
+    });
+
+    if (removedNote.length === notes.length ){
+        console.log(chalk.bgRed("No note found!"));
+    }else{
+        saveNotes(removedNote);
+        console.log(chalk.bgGreen("Note removed!"));
+    }
+}
+
 module.exports = {
     getNotes : getNotes,
-    addNote : addNote
+    addNote : addNote,
+    removeNote
 };
