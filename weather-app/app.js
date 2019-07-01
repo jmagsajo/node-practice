a7c32c433bb04df2497e1dfb1e58048fd78862ae
Joseph Magsajo <jmagsajo@yondu.com>
2019-07-01 16:34:57 +0800

    new commit

--- weather-app/app.js
+++ weather-app/app.js
@@ -29,8 +29,25 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/c5d45c1e6bc4664f05ad7c45e7ab9d7b/37.8267,-122.4233';
 
-request({ url: url }, (error, response) => {
+/* request({ url: url }, (error, response) => {
     const data = JSON.parse(response.body);
-    // console.log(response);
+    console.log(response);
     console.log(data.currently);
+}); */
+
+// https://www.mapbox.com/
+
+const mapUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGhlc29qMDMiLCJhIjoiY2p4azQ2cGpoMDRkMzNvb2dmZHljcjBxYyJ9.T3soGoqw5YYnSn94ubWxIw'
+const darkSkyUrl = 'https://api.darksky.net/forecast/c5d45c1e6bc4664f05ad7c45e7ab9d7b/';
+
+request({url : mapUrl+'&limit=1', json : true}, (error, response) => {
+    const center = response.body.features[0].center;
+    const lat = center[0];
+    const long = center[1];
+    console.log('lat: ' + lat + ' long: ' + long);
 });
+
+/* request({ url: url+'?lang=es', json: true }, (error, response) => {
+    const data = response.body.currently;
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + data.temperature + ' degress out. There is a ' + data.precipProbability + '% chance of rain');
+}); */
