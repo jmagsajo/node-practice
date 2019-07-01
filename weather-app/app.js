fd4969ade885032c8249d6fc219acd656e04be0f
Joseph Magsajo <magsajo.joseph@gmail.com>
2019-07-01 21:36:30 +0800

    new commit

--- weather-app/app.js
+++ weather-app/app.js
@@ -40,14 +40,35 @@ const url = 'https://api.darksky.net/forecast/c5d45c1e6bc4664f05ad7c45e7ab9d7b/3
 const mapUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGhlc29qMDMiLCJhIjoiY2p4azQ2cGpoMDRkMzNvb2dmZHljcjBxYyJ9.T3soGoqw5YYnSn94ubWxIw'
 const darkSkyUrl = 'https://api.darksky.net/forecast/c5d45c1e6bc4664f05ad7c45e7ab9d7b/';
 
-request({url : mapUrl+'&limit=1', json : true}, (error, response) => {
-    const center = response.body.features[0].center;
-    const lat = center[0];
-    const long = center[1];
-    console.log('lat: ' + lat + ' long: ' + long);
-});
-
 /* request({ url: url+'?lang=es', json: true }, (error, response) => {
     const data = response.body.currently;
     console.log(response.body.daily.data[0].summary + ' It is currently ' + data.temperature + ' degress out. There is a ' + data.precipProbability + '% chance of rain');
 }); */
+
+
+request({url : mapUrl+'&limit=1', json : true}, (error, response) => {
+    if(error){
+        console.log("Unable to connect to location services!");
+    }else if(response.body.message == 'Not Authorized - Invalid Token'){
+        console.log('Not Authorized - Invalid Token');
+    }else if(response.body.features.length === 0){
+        console.log('Unable to find location!');
+    }else{
+        const center = response.body.features[0].center;
+        const lat = center[0];
+        const long = center[1];
+        console.log('lat: ' + lat + ' long: ' + long);
+    }
+});
+
+/* request({ url: url+'?lang=es', json: true }, (error, response) => {
+    if(error){
+        console.log("Unable to connect to weather service!");
+    }else if(response.body.code){
+        console.log("Unable to find location!");
+    }else{
+        const data = response.body.currently;
+        console.log(response.body.daily.data[0].summary + ' It is currently ' + data.temperature + ' degress out. There is a ' + data.precipProbability + '% chance of rain');
+    }
+    
+});  */
