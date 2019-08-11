964ab7a6f5571f96e2cd2ddc1c4c08faa67c4097
Joseph Magsajo <jmagsajo@yondu.com>
2019-08-11 19:32:23 +0800

    new commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 // console.log(__dirname)
 // console.log(__filename)
@@ -8,7 +9,38 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//set-up handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//set-up static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+   res.render('index', {
+       'title': 'Weather',
+       'name': 'Joseph Magsajo'
+   })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        'title': 'About',
+        'name': 'Joseph Magsajo'
+    })
+ })
+
+ app.get('/help', (req, res) => {
+    res.render('help', {
+        'title': 'Help',
+        'message': 'This is a help message!',
+        'name': 'Joseph Magsajo'
+    })
+ })
 
 // app.get('', (req, res) => {
 //     res.send('<h1>Hello express!</h1>')
